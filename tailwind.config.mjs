c5dbf93ee3bbb95ccdd88578a320f6b4a8f00bd8
wuiquique <luisenriquem15@gmail.com>
2024-11-20 23:54:18 -0600

    MainLayout v1

---
+++ tailwind.config.mjs
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+        colors: {
+            dark: {
+                bg: "#16161A",
+                titles: "#FFFFFE",
+                texts: "#94A1B2",
+                primary: "#7F5AF0",
+                secondary: "#2CB67D",
+                strokes: "#010101",
+            },
+            light: {
+                bg: "#F3F3F3",
+                titles: "#000000",
+                texts: "#40454D",
+                primary: "#2CB67D",
+                secondary: "#B62C62",
+                strokes: "#FFFFFF",
+            },
+        },
+        fontFamily: {
+            titles: ['Hepta Slab', 'serif'],
+            texts: ['Roboto', 'sans-serif'],
+        }
+    },
+  },
+  plugins: [],
+};
